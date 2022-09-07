944e0fd876406ff4886709186f1bfe688ea19710
Fran-Cilia <francisco.cilia@gmail.com>
2022-09-06 17:58:10 -0700

    small change

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 const newTea = require('../controllers/teaController')
-const addCardController = require('../controllers/addCardController')
 
 router
     .route('/tea')
